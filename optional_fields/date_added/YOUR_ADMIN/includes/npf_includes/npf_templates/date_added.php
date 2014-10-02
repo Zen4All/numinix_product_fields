68ef3e52e291ff77f0660e51a0386f939a3f1750
numinix <jlew@numinix.com>
2014-10-02 09:57:32 -0700

    bug fix in date_added input field length

--- optional_fields/date_added/YOUR_ADMIN/includes/npf_includes/npf_templates/date_added.php
+++ optional_fields/date_added/YOUR_ADMIN/includes/npf_includes/npf_templates/date_added.php
@@ -4,7 +4,7 @@
           </tr>          
           <tr bgcolor="#DDEACC">
             <td class="main"><?php echo TEXT_PRODUCTS_DATE_ADDED; ?></td>
-            <td class="main"><?php echo zen_draw_separator('pixel_trans.gif', '24', '15') . '&nbsp;' . zen_draw_input_field('products_date_added', $pInfo->products_date_added, zen_set_field_length(TABLE_PRODUCTS, 'products_date_added')); ?></td>
+            <td class="main"><?php echo zen_draw_separator('pixel_trans.gif', '24', '15') . '&nbsp;' . zen_draw_input_field('products_date_added', $pInfo->products_date_added, 30); ?></td>
           </tr>
           <tr>
             <td colspan="2"><?php echo zen_draw_separator('pixel_trans.gif', '1', '10'); ?></td>
